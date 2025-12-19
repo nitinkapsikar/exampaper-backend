cdfe8165c22fe9782604fb9771f717b905bfe2e1
purushottam kapsikar <kapsikarpk@gmail.com>
2025-12-19 17:45:17 +0530

    Auto create superuser for Render free plan

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
 
-
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        from django.contrib.auth.models import User
+        if not User.objects.filter(is_superuser=True).exists():
+            User.objects.create_superuser(
+                username='admin',
+                password='admin123',
+                email='admin@example.com'
+            )
--- exampaperBackend/settings.py
+++ exampaperBackend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core.apps.CoreConfig',
     'rest_framework',
     'core',
 ]
