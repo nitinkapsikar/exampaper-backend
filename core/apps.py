97aae0c163e6641a5a88d235e928014bab0c5fce
purushottam kapsikar <kapsikarpk@gmail.com>
2025-12-19 18:29:33 +0530

    Remove auto superuser creation (caused migrate error)

--- core/apps.py
+++ core/apps.py
@@ -3,12 +3,3 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
-
-    def ready(self):
-        from django.contrib.auth.models import User
-        if not User.objects.filter(is_superuser=True).exists():
-            User.objects.create_superuser(
-                username='kapsikar',
-                password='nitin1234',
-                email='kapsikarpk@gmail.com'
-            )
